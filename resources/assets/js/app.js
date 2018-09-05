1ee2cda1a85532bb335c955f847747f560e09254
Mody Boss <ahmadsowiti@gmail.com>
2018-09-05 20:55:19 +0100

    update

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,12 +7,39 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import moment from 'moment';
+import { Form, HasError, AlertError } from 'vform'
+
+// ES6 Modules or TypeScript
+import swal from 'sweetalert2'
+window.swal = swal
+
+const toast = swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000
+});
+
+window.toast = toast
+
+window.Form = Form; //to access it in all components
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
 
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
+import VueProgressBar from 'vue-progressbar'
+Vue.use(VueProgressBar, {
+  color: 'rgb(143, 255, 199)',
+  failedColor: 'red',
+  height: '2px'
+})
+
 let routes = [
     { path: '/dashboard', component: require('./components/Dashboard.vue') },
+    { path: '/users', component: require('./components/Users.vue') },
     { path: '/profile', component: require('./components/Profile.vue') }
   ]
 
@@ -21,6 +48,15 @@ const router = new VueRouter({
     routes // short for `routes: routes`
   })
 
+  Vue.filter('upText', function(text){
+    return text.charAt(0).toUpperCase() + text.slice(1)
+  })
+
+  Vue.filter('myDate', function(created){
+    return moment(created).format('MMMM Do YYYY')
+  })
+
+window.Fire =  new Vue();
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -86,16 +86,16 @@ scratch. This page gets rid of all links and provides the needed markup only.
             
               <ul class="nav nav-treeview">
                 <li class="nav-item">
-                  <a href="#" class="nav-link active">
-                  <i class="fa fa-circle-o nav-icon"></i>
-                  <p>Active Page</p>
-                </a>
+                  <router-link to="/users" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>Users</p>
+                </router-link>
                 </li>
                 <li class="nav-item">
                   <a href="#" class="nav-link">
                   <i class="fa fa-circle-o nav-icon"></i>
                   <p>Inactive Page</p>
-                </a>
+                  </a>
                 </li>
               </ul>
             </li>
@@ -133,6 +133,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <div class="content">
         <div class="container-fluid">
             <router-view></router-view>
+            <!-- set progressbar -->
+            <vue-progress-bar></vue-progress-bar>
         </div>
         <!-- /.container-fluid -->
       </div>
